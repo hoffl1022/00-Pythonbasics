f6abcfc09654a74d539804e910d881546c3c863c
hoffl1022 <113646106+hoffl1022@users.noreply.github.com>
2022-09-30 10:30:03 +1300

    Add files via upload

---
+++ 02_variables.py
@@ -0,0 +1,12 @@
+# code to learn about variables
+
+# variables can be a string
+greeting = "hello world"
+
+# they can also be numbers
+fav_num = 54
+
+# Below is a constant
+PI = 3.14
+
+num_2 = 2
